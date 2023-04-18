ff55d326addcd4a3eb2790d6b5b6ca8034325443
Helen-A-lex <117782884+Helen-A-lex@users.noreply.github.com>
2023-04-18 15:12:49 +0000

    Deploying to gh-pages from @ Helen-A-lex/goit-js-hw-10@c3ae7264fc6cceb32d9e59ac7f5fe635ffcbe5c2 🚀

---
+++ index.822b66fa.js
@@ -0,0 +1,2 @@
+[document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info")][0].addEventListener("input",(function(e){console.log(e.currentTarget.value)}));
+//# sourceMappingURL=index.822b66fa.js.map
---
+++ index.f265ca2a.js
@@ -0,0 +1,2 @@
+({input:document.querySelector("#search-box"),countryList:document.querySelector(".country-list"),countryInfo:document.querySelector(".country-info")}).input.addEventListener("input",(function(e){console.log(e.currentTarget.value)}));
+//# sourceMappingURL=index.f265ca2a.js.map
