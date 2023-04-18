49f66b186af060bba9a3e147ed320dd44ad9bdf4
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-04-18 23:24:38 +0200

    Create fetchCountries.js

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,6 @@
+export function fetchCountries(name) {
+  const BASE_URL = `https://restcountries.com/v3.1/`;
+
+  // https://restcountries.com/v3.1/name/deutschland
+  fetch = `${BASE_URL}/name`;
+}
